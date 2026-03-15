48973c97dcc6e75df89aef1714d217ef42125221
Ahmed Aljasim <qiqw2024@gmail.com>
2026-03-15 08:36:19 +0300

    Complete JavaScript exercise

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
  * @returns {number} The sum of a and b.
  */
 function sum(a, b) {
-  // TODO: Implement this function.
+    return a + b;
+ 
 }
 
 /**
@@ -12,7 +13,8 @@ function sum(a, b) {
  * @returns {string} The reversed string.
  */
 function reverseString(str) {
-  // TODO: Implement this function.
+  return str.split("").reverse().join("");
+ 
 }
 
 /**
@@ -20,8 +22,13 @@ function reverseString(str) {
  * @returns {number} The largest number in the array. Return null if the array is empty.
  */
 function findLargest(numbers) {
-  // TODO: Implement this function.
+  if (numbers.length === 0) {
+    return null;
+  }
+  return Math.max(...numbers);
 }
+ 
+
 
 /**
  * @param {string} str The string to check.
@@ -30,16 +37,22 @@ function findLargest(numbers) {
  * The check should be case-insensitive.
  */
 function isPalindrome(str) {
-  // TODO: Implement this function.
+   const lower = str.toLowerCase();
+  const reversed = lower.split("").reverse().join("");
+  return lower === reversed;
 }
+ 
+
 
 /**
  * @param {number[]} numbers An array of numbers.
  * @returns {number[]} A new array containing only the even numbers from the original array.
  */
 function filterEvenNumbers(numbers) {
-  // TODO: Implement this function.
+   return numbers.filter(num => num % 2 === 0);
 }
+ 
+
 
 
 // Do not edit the line below.
